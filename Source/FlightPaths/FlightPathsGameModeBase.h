52334ee3f4078b5ac708a2469eea93fdcf22fb85
NMBLM <nunomblm.1997@gmail.com>
2023-03-01 19:05:24 +0000

    Initial Blank UE5 Project

---
+++ Source/FlightPaths/FlightPathsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlightPathsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTPATHS_API AFlightPathsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
